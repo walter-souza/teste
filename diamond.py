881ce887df7f1eece9327bc117cf852fe5c20f5d
walter-souza <41118985+walter-souza@users.noreply.github.com>
2020-11-10 10:38:09 -0300

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,15 +1,14 @@
 def draw(n):
-  
   for i in reversed(range(0,n)):
-    print(" " * i, end="#")
+    print(" " * i, end="#") # outside spaces + #
     if (int(2*(n - i) - 1) > 1):
-      print(" " * int(2*(n - i) - 3), end="#")
+      print(" " * int(2*(n - i) - 3), end="#") # inside spaces + #
     print()
 
   for i in range(1,n):
-    print(" " * i, end="#")
+    print(" " * i, end="#") # outside spaces + #
     if (int(2*(n - i) - 1) > 1):
-      print(" " * int(2*(n - i) - 3), end="#")
+      print(" " * int(2*(n - i) - 3), end="#") # inside spaces + #
     print()
 
   return
